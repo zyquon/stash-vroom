ff5946294a3adcbd96d2277691714ff72daac718
Zyqu Onrel <zyqu.onrel>
2025-05-27 16:45:12 -0500

    Somewhat working JAV parser

--- stash_vroom/jav.py
+++ stash_vroom/jav.py
@@ -46,11 +46,26 @@ def get_jav_info(filepath):
     if mtch[3] in years:
         # log.info(f'  - Not JAV for year match {repr(filename)}: "{mtch[1]}" "{mtch[2]}" {mtch[3]}')
         return None
+
     part = mtch[5] if mtch[5] else ''
     part = re.sub(r'^part', '', part, flags=re.IGNORECASE)
     part = part.upper()
+
+    # The release ID may have leading zeroes only to left-pad a 1 or 2 digit number. Otherwise, trim.
+    release_id = str(mtch[3])
+    while len(release_id) < 3:
+        release_id = '0' + release_id
+    while len(release_id) >= 4 and release_id[0] == '0':
+        release_id = release_id[1:]
+
     # log.debug(f'  - JAV match {repr(filename)}: "{mtch[1]}" "{mtch[2]}" {mtch[3]} {repr(mtch[4])} {repr(part)}')
-    return {'studio':mtch[1].upper(), 'id':mtch[3], 'mid':mtch[2], 'filename':filename, 'part':part}
+    return {
+        'studio': mtch[1].upper(),
+        'id'    : release_id,
+        'mid'   : mtch[2],
+        'part'  : part,
+        'filename': filename,
+    }
 
 def match_jav_filename(filename):
     if not filename or not isinstance(filename, str):
@@ -63,7 +78,7 @@ def match_jav_filename(filename):
         #log.debug(f'  - Not JAV; matched slr: {slr_info}')
         return None
 
-    if filename.endswith('-181_180x180_3dh_LR.mp4'):
+    if filename.endswith('-180_180x180_3dh_LR.mp4'):
         #log.debug(f'  - Not JAV; matched suffix')
         return None
 
@@ -77,7 +92,8 @@ def match_jav_filename(filename):
 
     # Fix all the WVR variants first.
     #fname = re.sub(r'^wvr0(\d)'                    , r'WVR1-\1', fname, flags=re.IGNORECASE)
-    fname = re.sub(r'^2DSVR\b'                     , r'DSVR'   , fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR-10*?(\D)'                 , r'WVR1\1', fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR-1(\d)'                   , r'WVR1\1', fname, flags=re.IGNORECASE)
 
     fname = re.sub(r'^WVR-11-(\d\d\d)'             , r'WVR1-\1', fname, flags=re.IGNORECASE)
     fname = re.sub(r'^WVR-2-(\d\d\d)'              , r'WVR1-\1', fname, flags=re.IGNORECASE)
@@ -86,18 +102,22 @@ def match_jav_filename(filename):
     fname = re.sub(r'^WVR-11(\d\d\d)'              , r'WVR1\1' , fname, flags=re.IGNORECASE)
     fname = re.sub(r'^WVR-2(\d)'                   , r'WVR1\1' , fname, flags=re.IGNORECASE)
 
-    fname = re.sub(r'^WVR5D-'                      , r'WVR6-'  , fname, flags=re.IGNORECASE)
-    fname = re.sub(r'^WVR5-D(\d)'                  , r'WVR6-\1', fname, flags=re.IGNORECASE)
-    fname = re.sub(r'^WVR5D(\d\d\d)'               , r'WVR6\1' , fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR6D-'                      , r'WVR6-'  , fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR6-D(\d)'                  , r'WVR6-\1', fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR6D(\d\d\d)'               , r'WVR6\1' , fname, flags=re.IGNORECASE)
 
-    fname = re.sub(r'^WVR7(\d\d\w)\b'              , r'WVR80\1',fname, flags=re.IGNORECASE)
-    fname = re.sub(r'^WVR-9(\d\d\d)'              , r'WVR8\1', fname, flags=re.IGNORECASE)
-    fname = re.sub(r'^WVR-9(\d\d\d)'               , r'WVR8\1', fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR8(\d\d\w)\b'              , r'WVR80\1',fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR-0*8'                     , r'WVR8', fname, flags=re.IGNORECASE)
 
-    fname = re.sub(r'^WVR8d\b'                     , r'WVR9'  , fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR-9(\d\d\d)'               , r'WVR9\1', fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR-9(\d\d\d)'               , r'WVR9\1', fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR9[cd]\b'                  , r'WVR9'  , fname, flags=re.IGNORECASE)
+    fname = re.sub(r'^WVR9[cd](\d)'                , r'WVR9\1', fname, flags=re.IGNORECASE)
     fname = re.sub(r'^WVR-91(\d\d\d)'              , r'WVR9\1', fname, flags=re.IGNORECASE)
 
     # Fix all the rest.
+    fname = re.sub(r'^3DSVR(\b|\d)'                , r'DSVR\1' , fname, flags=re.IGNORECASE)
+
     fname = re.sub(ok_extension_re                    , ''      , fname, flags=re.IGNORECASE)
     fname = re.sub(r'[-_\b]mkx199'                    , ''      , fname, flags=re.IGNORECASE)
     fname = re.sub(r'[-_\b]mkx219'                    , ''      , fname, flags=re.IGNORECASE)
@@ -108,14 +128,14 @@ def match_jav_filename(filename):
     #fname = re.sub(r'abc122', '', fname)
 
     #log.debug(f'-- fname = {repr(fname)}')
-    connector_re = r'[-_0\s\.]*'
-    part_re = f'({connector_re})' + r'(\d{0,2}\b|[a-z]\b|part\d+)'
+    connector_re = r'[-_\s\.]*'
+    part_re = f'({connector_re})' + r'(\d{1,2}\b|[a-z]\b|part\d+)'
     jav_re = (''
         + r'\b'
         #+ r'(2?[a-z]{3,9}(?:6|6D|8|9D)?)'
         #+ r'(2?[a-z]{3,9})'
         #+ r'(WVR0|WVR4|WVR8|WVR9|WVR6)'
-        + r'(WVR0|WVR4|WVR8|WVR9|WVR6|[a-z]{3,9})'
+        + r'(WVR0|WVR1|WVR4|WVR8|WVR9|WVR6|[a-z]{3,9})'
         + f'({connector_re})'
         + r'(\d{2,6})'
         + r'(?:' + part_re + r')?'
---
+++ tests/tests_jav.py
@@ -0,0 +1,72 @@
+import stash_vroom.jav as jav
+
+def test_get_jav_info():
+    tests = (
+        ('SkinRays_SunsetDance_180_LR.mp4'             , None                                 ),
+        ('JillVR_1234.mp4'                             , None                                 ),
+        ('CBIKMV-068.24399-SLR.mp4'                    , ('CBIKMV'   , '-' , '068'   , ''    )),
+        ('dandyhqvr-011-b.MP4'                         , ('DANDYHQVR', '-' , '011'   , 'B'   )),
+        ('DANDYHQVR 015 A.mp4'                         , ('DANDYHQVR', ' ' , '015'   , 'A'   )),
+        ('pxvr00051.part1.mp4'                         , ('PXVR'     , ''  , '051'   , '1'   )),
+        ('ebvr00099-3.mp4'                             , ('EBVR'     , ''  , '099'   , '3'   )),
+        ('cafr333.mp4'                                 , ('CAFR'     , ''  , '333'   , ''    )),
+        ('realhotvr-example-180_180x180_3dh_LR.mp4'    , None),
+        ('reality-lovers-example-180_180x180_3dh_LR.mp4',None),
+        ('sexbabesvr-example-180_180x180_3dh_LR.mp4'   , None),
+        ('only3xvr-example-180_180x180_3dh_LR.mp4'     , None),
+        ('wankzvr-example-180_180x180_3dh_LR.mp4'      , None),
+        ('SLR-AsianSexVR-Title-1920p-48493-LR-180.mp4' , None),
+        ('TMAVR-200-1.SLR_TMAVR_Filename Part 1_2160p_47574_LR_180.mp4', ('TMAVR', '-', '200', '1')),
+        ('CAFR-219.SLR_CasanovA_Example Title_1920p_1234_LR_180.mp4'   , ('CAFR' , '-', '219', '' )),
+
+        ('wvr1001A.VR.mp4'                             , ('WVR1'     , ''  , '001'   , 'A'   )),
+        ('WVR-1001-2.mp4'                              , ('WVR1'     , ''  , '001'   , '2'   )),
+        ('WVR-100001.avi'                              , ('WVR1'     , ''  , '001'   , ''    )),
+        ('WVR-1-001.avi'                               , ('WVR1'     , '-' , '001'   , ''    )),
+        ('WVR-10-001.avi'                              , ('WVR1'     , '-' , '001'   , ''    )),
+        ('WVR6D-044_32360-SLR.mp4'                     , ('WVR6'     , '-' , '044'   , ''    )),
+        ('wvr6d014.VR.mp4'                             , ('WVR6'     , ''  , '014'   , ''    )),
+        ('WVR6-D093.mp4'                               , ('WVR6'     , '-' , '093'   , ''    )),
+        ('WVR8-006.mp4'                                , ('WVR8'     , '-' , '006'   , ''    )),
+        ('wvr801A.VR.mp4'                              , ('WVR8'     , ''  , '001'   , 'A'   )),
+        ('WVR-08002-A.mp4'                             , ('WVR8'     , ''  , '002'   , 'A'   )),
+        ('WVR-8002-2.mp4'                              , ('WVR8'     , ''  , '002'   , '2'   )),
+        ('WVR-90009 Test VR3D_4K_LR_180.mp4'           , ('WVR9'     , ''  , '009'   , ''    )),
+        ('wvr9c018A.VR.mp4'                            , ('WVR9'     , ''  , '018'   , 'A'   )),
+        ('wVr9d.078 VR Foo bar - baz 2048p_180_3dh.mp4', ('WVR9'     , '.' , '078'   , ''    )),
+    )
+
+    for test in tests:
+        filename, expected = test
+        e_studio, e_mid, e_id, e_part = expected if expected else (None, None, None, None)
+
+        jav_info = jav.get_jav_info(f'/example/{filename}')
+
+        if not jav_info and expected is None:
+            continue
+        elif jav_info and expected is None:
+            raise Exception(f'Expected None for {filename}, but got: {jav_info}')
+        elif not jav_info and expected:
+            raise Exception(f'Got None for {filename}, but expected: {expected}')
+        else: # both jav and expected
+            r_studio, r_mid, r_id, r_part = None, None, None, None
+
+            if jav_info['studio'] != e_studio:
+                r_studio = repr(jav_info['studio']) + ' vs ' + repr(e_studio)
+
+            if jav_info['mid'] != e_mid:
+                r_mid = repr(jav_info['mid']) + ' vs ' + repr(e_mid)
+
+            if jav_info['id'] != e_id:
+                r_id = repr(jav_info['id']) + ' vs ' + repr(e_id)
+
+            if jav_info['part'] != e_part:
+                r_part = repr(jav_info['part']) + ' vs ' + repr(e_part)
+
+            if r_studio or r_mid or r_id or r_part:
+                raise Exception(f'For {filename}:\n'
+                                f'  Studio: {r_studio}\n'
+                                f'  MID   : {r_mid}\n'
+                                f'  ID    : {r_id}\n'
+                                f'  Part  : {r_part}\n'
+                                f'Got: {jav_info}')
