5006b8f4c2ab9936625cda5b3d6c25f96aa0477e
Zyqu Onrel <zyqu.onrel>
2025-06-25 15:26:24 -0500

    Doc updates

--- doc/design.rst
+++ doc/design.rst
@@ -13,13 +13,13 @@ Philosophy
 - Browsing VR Stash content with HereSphere should be easy and intuitive
 - Help everyday users browse their VR Stash, and also help developers build similar technology
 
-To make VR on Stash easy, **VRoom will be a Stash plugin**: easy to install, easy to run, easy to configure.
+To make VR on Stash easy, **VRoom is a Stash plugin**: easy to install, easy to run, easy to configure.
 VRoom aspires to be a reliable, mature tool providing all the services needed to
 manage VR Stash content.
 
 To make the HereSphere experience convenient and intuitive, VRoom has no UI and minimal configuration.
 VRoom should be transparent. HereSphere should just be a nice new way to browse our Stash we know and love.
-To change what see in HereSphere, change your data in Stash. VRoom makes HereSphere reflect your Stash content directly:
+To change what you see in HereSphere, change your data in Stash. VRoom makes HereSphere reflect your Stash content directly:
 - Stash **Saved Filters** become HereSphere **Libraries**. To see VR scenes in HereSphere, just make a Filter called ``VR``. You will recognize the Filter view in HereSphere.
 - Scene **Tags** become HereSphere **Tags**. You can browse them in HereSphere, and click to filter scenes by tag.
 - Scene **Markers** become HereSphere **Tags** also. (Yes, HereSphere reuses tags for this.) VRoom makes it right. You can search, jump, and autoseek to any marker. If you make VRoom read-write, as you add and edit HereSphere tags, they are saved as Stash scene markers. (This can be very fast and powerful.)
--- stash_vroom/changes.py
+++ stash_vroom/changes.py
@@ -17,25 +17,27 @@ This module provides a placeholder for managing scene-related events,
 similar to the Node.js EventEmitter object.
 """
 
+# TODO: https://github.com/pyapp-kit/psygnal?tab=readme-ov-file#evented-containers
+
 import re
 import logging
+import psygnal
 from typing import Callable, Dict, List
 
 log = logging.getLogger(__name__)
 
 class SceneChanges:
     """
-    A placeholder class for managing scene-related events.
+    A class for managing scene-related events using psygnal.
     """
 
-    def __init__(self, stash_api, blacklist=None):
-        self._event_handlers: Dict[str, List[Callable]] = {
-            "insert_view": [],
-            "delete_view": [],
-            "insert_scene": [],
-            "delete_scene": []
-        }
+    # Define signals for events
+    insert_view = psygnal.Signal(object)  # Signal with a single argument (e.g., a view object)
+    delete_view = psygnal.Signal(object)
+    insert_scene = psygnal.Signal(object)
+    delete_scene = psygnal.Signal(object)
 
+    def __init__(self, stash_api, blacklist=None):
         self.stash = stash_api
         self.blacklist = blacklist or []
 
@@ -43,225 +45,25 @@ class SceneChanges:
         self.scenes = {}
         log.info("Initialized SceneChanges")
 
-    def on(self, event_name: str, handler: Callable):
-        """
-        Register an event handler for a specific event.
-
-        :param event_name: Name of the event
-        :param handler: Callable to handle the event
-        """
-        if event_name not in self._event_handlers:
-            raise ValueError(f"Unsupported event: {event_name}")
-        self._event_handlers[event_name].append(handler)
-        # log.debug(f"Registered handler for event {event_name}: {handler.__name__}")
-
-    def emit(self, event_name: str, *args, **kwargs):
-        """
-        Trigger an event and call all registered handlers.
-
-        :param event_name: Name of the event
-        :param args: Positional arguments to pass to the handlers
-        :param kwargs: Keyword arguments to pass to the handlers
-        """
-        if event_name not in self._event_handlers:
-            raise ValueError(f"Unsupported event: {event_name}")
-        for handler in self._event_handlers[event_name]:
-            try:
-                handler(*args, **kwargs)
-            except Exception as e:
-                log.error(f"Error in handler for event {event_name}: {e}")
-                raise
-            else:
-                log.debug(f"Executed handler for event {event_name}: {handler.__name__}")
-    
     def start(self):
         """
         Start the SceneChanges event manager.
         """
         log.info("Start SceneChanges")
         self.boot_scenes()
-    
-    def boot_scenes(self):
-        # Image filtering disabled for now.
-        # img_filters = STASH.find_saved_filters(mode='IMAGES')
-        # log.debug(f'Image filters found: {len(img_filters)}')
-        img_filters = []
 
+    def boot_scenes(self):
+        """
+        Load scenes and views, emitting signals for each event.
+        """
         scene_filters = self.stash.find_saved_filters(mode='SCENES')
         log.debug(f'Scene filters found: {len(scene_filters)}')
 
-        all_filters = scene_filters + img_filters
+        for flt in scene_filters:
+            if flt['name'] in self.blacklist:
+                log.debug(f'Skip blacklisted filter: {flt["name"]}')
+                continue
 
-        stash_filters = []
-        for flt in all_filters:
-            ok_prefixes = r'AA|XP|VR' # Should be just "VR" I think.
-            vr_re = r'^\s*(' + ok_prefixes + r')\s*\|\s*(.+?)\s*$'
-            match = re.match(vr_re, flt['name'], re.IGNORECASE)
-            if match:
-                filter_name = match.group(1)
-            elif flt['name'].strip().lower() == 'vr':
-                filter_name = flt['name'].strip()
-            else:
-                filter_name = None
-            
-            if filter_name:
-                if flt['name'] in self.blacklist:
-                    log.debug(f'Skip blacklisted filter: {filter_name}')
-                else:
-                    stash_filters.append(flt)
-
-        all_st_scenes = {}
-        img_fragment = get_big_image_fragment()
-        scene_fragment = get_big_scene_fragment()
-        for flt in sorted(stash_filters, key=lambda x: x['name'].lower()):
-            kwargs = saved_filter_to_find_scenes_kwargs(flt)
-
-            scenes = []
-            images = []
-
-            log.debug(f'Filter: {flt["name"]}')
-            if flt.get('mode') == 'IMAGES':
-                images = self.stash.find_images(fragment=img_fragment, **kwargs)
-            elif '_scene_ids' in flt:
-                scenes = self.stash.get_scene(id=flt['_scene_ids'], fragment=scene_fragment)
-            elif not all_st_scenes:
-                # This API call is slightly faster per call, so it is more useful for a cold start, ideally
-                # for an "All" view to pull down most scenes.
-                log.debug(f'Get scenes for filter: {flt["name"]}')
-                scenes = self.stash.find_scenes(fragment=scene_fragment, **kwargs)
-                for scene in scenes:
-                    all_st_scenes[ scene['id'] ] = scene
-            else:
-                # This method is faster to fill in a few scenes by ID.
-                scenes = self.stash.find_scenes(fragment='id', **kwargs)
-                all_scene_ids = [ X['id'] for X in scenes ]
-                unknown_scene_ids = [ X for X in all_scene_ids if X not in all_st_scenes ]
-                log.debug(f'View {repr(flt["name"])} returned: {len(all_scene_ids)} IDs, {len(all_scene_ids) - len(unknown_scene_ids)} known, query for {len(unknown_scene_ids)} new')
-                new_scenes = self.stash.get_scene(id=unknown_scene_ids, fragment=scene_fragment)
-
-                # Build the scenes list from the known and new scenes, maintaining order.
-                for scene_id in all_scene_ids:
-                    if scene_id in all_st_scenes:
-                        scene = all_st_scenes[scene_id]
-                    elif scene_id == new_scenes[0]['id']:
-                        scene = new_scenes.pop(0)
-                    else:
-                        raise Exception(f'Expected scene {scene_id} to be this scene: {new_scenes[0]}')
-                    scenes.append(scene)
-
-            for scene in scenes:
-                all_st_scenes[ scene['id'] ] = scene
-
-            log.debug(f'Scenes in filter {flt["name"]}: {len(scenes)}')
-            log.debug(f'Total scenes known: {len(all_st_scenes)}')
-
-def saved_filter_to_find_scenes_kwargs(flt):
-    # TODO: Maybe return the GQL object rather than Stashapp API but this is good enough.
-    find_filter = flt['find_filter']
-    object_filter = flt['object_filter']
-    filter_to_query(object_filter)
-
-    # Always query for the entire data set without paging.
-    find_filter['page'] = 1
-    find_filter['per_page'] = -1
-
-    # view_id = flt['name']
-    kwargs = {
-        'f': object_filter,
-        'q': find_filter['q'],
-        'filter': find_filter,
-    }
-    return kwargs
-
-def filter_to_query(flt):
-    """
-    Convert a saved filter object into a query object suitable for finding scenes over GraphQL
-    """
-    #log.debug(f'Fix filter:\n' + json.dumps(flt, indent=2))
-    tag_labels = ['tags', 'performer_tags']
-    for tag_label in tag_labels:
-        if tag_label in flt:
-            original = flt[tag_label]
-
-            flt[tag_label] = {}
-            flt[tag_label]['depth'] = original['value']['depth']
-            flt[tag_label]['modifier'] = original['modifier']
-
-            if 'items' in original['value']:
-                flt[tag_label]['value'] = [ X['id'] for X in original['value']['items'] ]
-            if 'excluded' in original['value']:
-                flt[tag_label]['excludes'] = [ X['id'] for X in original['value']['excluded'] ]
-
-    if 'studios' in flt:
-        modifier = flt['studios'].get('modifier')
-        if modifier != 'INCLUDES':
-            raise NotImplementedError(f'Modifier must be INCLUDES for now')
-
-        val = flt['studios'].get('value')
-        if not val:
-            raise ValueError(f'Filter .studios must have .value property')
-
-        depth = val.get('depth')
-        if depth != 0:
-            raise ValueError(f'Filter .studios depth must be 0')
-
-        excludes = val.get('excluded')
-        if not isinstance(excludes, list) or len(excludes) != 0:
-            raise ValueError(f'Filter for .studios excludes must be empty')
-
-        studio_ids = [ X['id'] for X in val['items'] ]
-
-        flt['studios'] = {
-            'depth': depth,
-            'modifier': modifier,
-            'excludes': excludes,
-            'value': studio_ids,
-        }
-
-    if 'is_missing' in flt:
-        if isinstance(flt['is_missing'], dict):
-            if flt['is_missing']['modifier'] != 'EQUALS':
-                raise Exception(f'Unknown is_missing: {flt}')
-            flt['is_missing'] = flt['is_missing']['value']
-
-    if 'has_markers' in flt:
-        if isinstance(flt['has_markers'], dict):
-            if flt['has_markers']['modifier'] != 'EQUALS':
-                raise Exception(f'Unknown has_markers: {flt}')
-            flt['has_markers'] = flt['has_markers']['value']
-
-    if 'performer_favorite' in flt:
-        if flt['performer_favorite']['modifier'] != 'EQUALS':
-            raise Exception(f'Unknown performer favorite value: {repr(flt)}')
-        flt['performer_favorite'] = flt['performer_favorite']['value'] in ('true', True)
-
-    keys = ['file_count', 'performer_count', 'rating100', 'o_counter']
-    for key in keys:
-        if key in flt:
-            # Convert "value":{"value":N} -> "value":N.
-            value = flt[key]['value']
-            if isinstance(value, dict) and 'value' in value:
-                flt[key]['value'] = value['value']
-
-def get_big_image_fragment():
-    fragment = ( ''
-        + ' __typename'
-        + ' id urls title details rating100 date created_at o_counter'
-        + ' paths { thumbnail image } tags { id name }'
-        + ' visual_files { __typename ... on ImageFile { basename path size width height } }'
-        + ' performers{ id name gender country favorite ethnicity tags{ name }  }'
-        + ' studio{ name tags{ id name } parent_studio{ name parent_studio{ name parent_studio{ name } } } }'
-        + '' )
-    return fragment
-
-def get_big_scene_fragment():
-    fragment = (
-        ' id urls title details rating100 date created_at o_counter play_count'
-        ' studio{ name tags{ id name } parent_studio{ name parent_studio{ name parent_studio{ name } } } }'
-        ' paths{ stream screenshot preview }'
-        ' files{ format basename size width height duration fingerprints{ type value } }'
-        ' performers{ id name gender country favorite ethnicity fake_tits tags{ name }  }'
-        ' scene_markers{ id seconds primary_tag{ name } tags{ id name parents{ id name parents{ id name parents{ id name } } } } }'
-        ' tags{ id name parents{ id name parents{ id name parents{ id name } } } }'
-    )
-    return fragment
+            # Example: Emit an insert_view signal
+            self.insert_view.emit(flt)
+            log.debug(f'Emitted insert_view for filter: {flt["name"]}')
