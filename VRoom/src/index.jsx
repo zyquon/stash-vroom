aaa8b9e3121c2e05af08775938506a5c1e9b4dbf
Zyqu Onrel <zyqu.onrel>
2025-06-28 05:02:56 -0500

    Run: npx yarn create stash-plugin

---
+++ VRoom/src/index.jsx
@@ -0,0 +1,10 @@
+/// <reference path="@types/pluginApi.d.ts" />
+
+import Button from "./components/Button/Button"
+
+export default () => {
+  console.log(Button())
+}
+
+// To obtain type support for PluginApi.libraries, simply install its respective "@types/library" or the library itself.
+// These libraries will not be bundled, as stash-plugin-builder will automatically maps them to PluginApi.libraries.
