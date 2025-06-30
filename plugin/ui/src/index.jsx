cd48e30a629435f6fbe3802a960cbfb715670727
Zyqu Onrel <zyqu.onrel>
2025-06-30 03:39:06 -0500

    No need for the bounce workaround if the path is /plugins/* and not /plugin/*

--- plugin/ui/src/index.jsx
+++ plugin/ui/src/index.jsx
@@ -7,39 +7,21 @@ const { Button } = PluginApi.libraries.Bootstrap;
 const { NavLink } = PluginApi.libraries.ReactRouterDOM;
 const { faVrCardboard } = PluginApi.libraries.FontAwesomeSolid;
 
-const STATIC_AND_REACT_PATH = `/plugin/VRoom/assets/static/VRoom.html`;
+const PATH = `/plugins/VRoom`
 
 // import MainNavBar from "./components/MainNavBar";
 import PluginHome from "./PluginHome.jsx"
 
-const Bounce = (props) => {
-  const { useHistory } = PluginApi.libraries.ReactRouterDOM
-  const history = useHistory();
-  history.push(props.to)
-  return null
-}
-
 export default () => {
   const { Icon } = PluginApi.components;
 
-  PluginApi.register.route(STATIC_AND_REACT_PATH, PluginHome)
-
-  PluginApi.patch.instead('App', (props, _, Original) => {
-    const match = (window.location.hash || "").match(/^#(\/.*)$/)
-    if (match) {
-      const new_path = match[1];
-      console.log(`Redirect from hash: ${window.location.hash} to path: ${new_path}`);
-      return <Bounce to={new_path} />
-    }
-
-    return [ <Original {...props} /> ]
-  })
+  PluginApi.register.route(PATH, PluginHome)
 
   PluginApi.patch.before("MainNavBar.UtilityItems", (props) => [
     {
       children: (
         <>
-          <NavLink className="nav-utility" to={STATIC_AND_REACT_PATH}>
+          <NavLink className="nav-utility" to={PATH}>
             <Button className="minimal d-flex align-items-center h-100 btn btn-primary" title="VRoom hello world tooltip?">
               <Icon icon={faVrCardboard} />
             </Button>
